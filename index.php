bc51f0e3b2137879ed08bec894dc7b15dee737b3
CAIQUE HENRIQUE BELTRAO <987308465@itaud.des.ihf>
2021-02-02 14:31:34 -0300

    atualizando site

--- index.php
+++ index.php
@@ -2,7 +2,7 @@ palmeiras nao tem mundial
 
 <div style="display:flex; height: 400px; width:100%;color: #8c8c8c;float-size: 300px;">
     <div style="background-color: blue;padding: 30px; text-align:center;width: 50%;">
-        essa é a coluna da cor amarela
+        essa é a coluna da cor azul
     </div>
 </div>
 <div style="background-color: yellow;padding: 30px; text-align:center;width: 50%">
