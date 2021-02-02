3c84f0e819e2fe4d6e26188e8b623ed1df89f2d0
CAIQUE HENRIQUE BELTRAO <987308465@itaud.des.ihf>
2021-02-02 14:08:15 -0300

    atualizando site

--- index.php
+++ index.php
@@ -1,2 +1,10 @@
 palmeiras nao tem mundial
 <h1>caue engravidou a praminha</h1>
+<div style="display:flex: height: 400px">
+    <div style="background-color: blue">
+        essa é a coluna da cor amarela
+    </div>
+</div>
+<div style="background-color: yellow">
+    essa é a coluna da cor amarela
+</div>
